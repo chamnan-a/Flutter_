7afe98a5d4b1c78769791b25e47829a6fb0e59c4
chamnan-a <148676128+chamnan-a@users.noreply.github.com>
2024-10-13 15:22:19 +0700

    Update EX-3.dart

--- W1-S2-Practice/EX-3.dart
+++ W1-S2-Practice/EX-3.dart
@@ -1,9 +1,9 @@
 void main() {
   // List of student scores
   List<int> scores = [45, 67, 82, 49, 51, 78, 92, 30];
+
+  // You code
   var pass=scores.where((score)=>score>50);
   print(pass.toList());
   print('Student who pass:${pass.length}');
-  // You code
-  
 }
