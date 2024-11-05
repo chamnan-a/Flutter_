599ab0c219d503cbca8e3f015c8d90dcce162bf9
chamnan-a <chamnankheng231@gmail.com>
2024-11-05 23:37:16 +0700

    stateful

--- flutter_practice/lib/W4-S3 Stateful Widget-PRACTICE/EX-2/ex_2_step_1_correction.dart
+++ flutter_practice/lib/W4-S3 Stateful Widget-PRACTICE/EX-2/ex_2_step_1_correction.dart
@@ -9,66 +9,41 @@ void main() => runApp(MaterialApp(
         ),
         body: Column(
           children: [
-            FavoriteCard(title: "Card1"),
-            FavoriteCard(title: "Card2")
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: .5, color: Colors.grey),
+                ),
+              ),
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Expanded(
+                    flex: 7,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'title',
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.w800),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text('description')
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ))
+                ],
+              ),
+            )
           ],
         ),
       ),
     ));
-
-class FavoriteCard extends StatefulWidget {
-  const FavoriteCard({
-    super.key,required this.title,this.description="description"
-  });
-  final String title;
-  final String description;
-  @override
-  State<FavoriteCard> createState() => _FavoriteCardState();
-}
-
-class _FavoriteCardState extends State<FavoriteCard> {
-  bool isFavorite=false;
-  void selectButton(){
-    setState(() {
-      isFavorite=!isFavorite;
-    });
-  }
-  Color get favoriteColor=> isFavorite?Colors.red:Colors.grey;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(width: .5, color: Colors.grey),
-        ),
-      ),
-      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-           Expanded(
-            flex: 7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.title,
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.w800),
-                ),
-                SizedBox(height: 10.0),
-                Text(widget.description)
-              ],
-            ),
-          ),
-          IconButton(
-              onPressed: selectButton,
-              icon:  Icon(
-                Icons.favorite,               
-                color: favoriteColor,
-              ))
-        ],
-      ),
-    );
-  }
-}
