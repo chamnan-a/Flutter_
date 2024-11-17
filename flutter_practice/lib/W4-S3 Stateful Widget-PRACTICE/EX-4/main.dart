1157fda58719ff5e8e32afa07db657bbe030a1b0
chamnan-a <chamnankheng231@gmail.com>
2024-11-17 22:03:18 +0700

    Revert "add w6 s2"
    This reverts commit 44658ca07da145f154491971a9d778a956ec5b8e, reversing
    changes made to ccd3e164d773a66eac67829982baf5b705ea80a2.

--- flutter_practice/lib/W4-S3 Stateful Widget-PRACTICE/EX-4/main.dart
+++ flutter_practice/lib/W4-S3 Stateful Widget-PRACTICE/EX-4/main.dart
@@ -4,15 +4,10 @@ void main() {
   runApp(MyApp());
 }
 
-<<<<<<< HEAD
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-=======
-class _ScoreCardState extends State<ScoreCard> {
-  double score = 0; // Initial score value from 0
->>>>>>> 5e963f10ab84615d5fe006f01d26a14b34216ba5
 
 class _MyAppState extends State<MyApp> {
   bool _isScreenA = true; // Boolean to track current screen
@@ -24,7 +19,6 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-<<<<<<< HEAD
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,100 +30,6 @@ class _MyAppState extends State<MyApp> {
           child: _isScreenA
               ? ScreenA(onSwitch: _switchScreen) // Show Screen A
               : ScreenB(onSwitch: _switchScreen), // Show Screen B
-=======
-  // Function to decrease score
-  void decreaseScore() {
-    setState(() {
-      if (score > 0) {
-        score--;
-      }
-    });
-  }
-
-  // Function to calculate the width for the progress bar
-  double calculateProgressBarWidth(double score, double maxWidth) {
-      //set limit score to be range from 1-10
-    if (score < 0) score = 0;
-    if (score > 10) score = 10;
-    return (score / 10.0) * maxWidth; //normalize the score 
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 180,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                widget.text,
-                style: const TextStyle(fontSize: 28),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    child: ElevatedButton(
-                      onPressed: decreaseScore,
-                      child: const Icon(Icons.remove),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  SizedBox(
-                    child: ElevatedButton(
-                      onPressed: increaseScore,
-                      child: const Icon(Icons.add),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            //use layout builde widget for working both chrome and emulator
-            LayoutBuilder(
-              builder: (context, constraints) {
-                 //the maxwidth alway minus 8 to make it look stay inside the border container (the left and right width)
-                double progressWidth = calculateProgressBarWidth(score, constraints.maxWidth - 8);
-                return Stack(
-                  fit: StackFit.loose,
-                  children: [
-                    // Border Container
-                    Container(
-                      width: constraints.maxWidth,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.black, width: 4),
-                      ),
-                    ),
-                    // Progress Container
-                    Positioned(
-                      top: 4,
-                      left: 4,
-                      child: Container(
-                        width: progressWidth,
-                        height: 22,
-                        decoration: BoxDecoration(
-                          color: widget.colorBar,
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ],
->>>>>>> 5e963f10ab84615d5fe006f01d26a14b34216ba5
         ),
       ),
     );
